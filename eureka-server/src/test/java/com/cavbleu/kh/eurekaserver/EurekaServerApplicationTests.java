6d18de09a6a60a4fe7bb15c12556234881db2dac
cavbleu <56340634+cavbleu@users.noreply.github.com>
2024-01-02 20:43:07 +0700

    Add files via upload

---
+++ eureka-server/src/test/java/com/cavbleu/kh/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavbleu.kh.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
