f57cbb31f5eb8dcfa54836667b65b2f1ff9b8df3
cavbleu <56340634+cavbleu@users.noreply.github.com>
2024-01-02 20:33:45 +0700

    Add files via upload

---
+++ src/main/Sensor.java
@@ -0,0 +1,23 @@
+package com.cavbleu.kh.webcontroller.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author cavbleu
+ * @project cloud-sensor
+ * @create 2023-11-22 19:35
+ */
+
+@Data
+@Setter
+@Getter
+@NoArgsConstructor
+public class Sensor {
+
+    private Long id;
+
+    private String names;
+}
